f7c7ad21c31f5fb695e27fc99c337a07ba3eb78e
egbean <1358404767@qq.com>
2026-09-10 20:28:12 +0800

    调整阴影样式

--- src/main/java/com/sojourners/chess/board/CustomBoardRender.java
+++ src/main/java/com/sojourners/chess/board/CustomBoardRender.java
@@ -84,13 +84,14 @@ public class CustomBoardRender extends BaseBoardRender {
 
         double shadowOffsetX = -piece * 0.08;
         double shadowOffsetY = piece * 0.14;
-        double shadowRadius = r * 0.95;
+        double shadowRadius = r * 1;
 
         javafx.scene.paint.RadialGradient shadowGradient = new javafx.scene.paint.RadialGradient(
                 0, 0, 0.5, 0.5, 0.5, true, javafx.scene.paint.CycleMethod.NO_CYCLE,
                 new javafx.scene.paint.Stop(0.0, javafx.scene.paint.Color.rgb(0, 0, 0, 1.0)),
-                new javafx.scene.paint.Stop(0.55, javafx.scene.paint.Color.rgb(0, 0, 0, 0.98)),
-                new javafx.scene.paint.Stop(0.80, javafx.scene.paint.Color.rgb(0, 0, 0, 0.70)),
+                new javafx.scene.paint.Stop(0.60, javafx.scene.paint.Color.rgb(0, 0, 0, 0.98)),
+                new javafx.scene.paint.Stop(0.78, javafx.scene.paint.Color.rgb(0, 0, 0, 0.55)),
+                new javafx.scene.paint.Stop(0.88, javafx.scene.paint.Color.rgb(0, 0, 0, 0.0)),
                 new javafx.scene.paint.Stop(1.0, javafx.scene.paint.Color.rgb(0, 0, 0, 0.0))
         );
         gc.setFill(shadowGradient);
